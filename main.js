b6b72bd3a568a29a6412bf36e7b65ba477c4681b
Annabelle Zogaib <azogaib@ufl.edu>
2021-11-07 23:56:43 -0500

    Update index.html

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+// Create Variables from form
+var formEl = document.querySelector('form')
+var searchInput = document.querySelector('input')
+var weather = document.getElementById('weather')
+console.log (formEl, searchInput, weather)
+
+// Fetch data using variables
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=miami&units=imperial&appid=32eb3edd83ffd4be71f34fe205739f96')
+    .then(function(res){
+        return res.json()
+    })
+    .then(function(res){
+    console.log(res)    
+})
+
+// Display data
+
+
+
+
 // Name of the Searched City with Country Code
 // Description of current weather
 // Icon image for current weather conditions
