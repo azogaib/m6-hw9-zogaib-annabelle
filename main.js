18c1b2490c07a83730f06368b1125e10fd7128b5
Annabelle Zogaib <azogaib@ufl.edu>
2021-11-07 21:38:34 -0500

    Set up documents

---
+++ main.js
@@ -0,0 +1,7 @@
+// Name of the Searched City with Country Code
+// Description of current weather
+// Icon image for current weather conditions
+// Current Temperature
+// Current "feels like" Temperature
+
+// api.openweathermap.org/data/2.5/weather?q=YOURQUERY&units=imperial&appid=YOURAPIKEY
